49843a1dee4e33ee3ab52ed94a72b772fe7a834a
Jean Aragão <aragao.jean@gmail.com>
2025-04-06 08:49:14 -0300

    fix transaction create

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -14,15 +14,26 @@ class TransactionsController < ApplicationController
   def create
     @transaction = Transaction.new(transaction_params)
     if @transaction.save
-      redirect_to transactions_path, notice: "Transaction created successfully."
+      flash.now[:notice] = "Transaction was successfully created."
+      render turbo_stream: [
+      turbo_stream.prepend("transaction", @transaction),
+      turbo_stream.replace("form_transaction", partial: "form", locals: { transaction: Transaction.new }
+      ),
+      turbo_stream.replace("notice", partial: "layouts/flash")
+    ]
     else
-      render :new
+      flash.now[:alert] = @transaction.errors.full_messages.join(", ")
+      render turbo_stream: [
+      turbo_stream.replace("notice", partial: "layouts/flash")
+    ], status: :unprocessable_entity
     end
+
+    puts @transaction.errors.full_messages
   end
 
   private
 
   def transaction_params
-    params.require(:transaction).permit(:book_id, :amount, :transaction_type)
+    params.require(:transaction).permit(:date, :book_id, :amount, :transaction_type)
   end
 end
