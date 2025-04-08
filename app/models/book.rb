7d1d5e29bdc6502ca988aee74e14e03f85720261
Jean Aragão <aragao.jean@gmail.com>
2025-04-07 21:56:57 -0300

    add better colors on books list table

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,31 +1,30 @@
 class Book < ApplicationRecord
-  belongs_to :user 
-  validates :owner, presence: true
-  validates :name, presence: true
+  belongs_to :user
+
   has_many :transactions, dependent: :destroy
   has_many :daily_balances, dependent: :destroy
+
+  validates :owner, presence: true
+  validates :name, presence: true
 
   before_validation :set_description
 
   def total_deposit
-    daily_balances.where("balance > 0").sum(:balance)
+    transactions.where(transaction_type: 'deposit').sum(:amount)
   end
-  
+
   def total_withdraw
-    daily_balances.where("balance < 0").sum(:balance)
+    transactions.where(transaction_type: 'withdraw').sum(:amount)
   end
-  
+
   def last_daily_balance
     daily_balances.order(date: :desc).limit(1).pluck(:balance).first
   end
-  
-  def balance
-    transactions.sum { |t| t.transaction_type == "withdraw" ? -t.amount : t.amount }
-  end
-  
+
   private
 
   def set_description
-    self.description = "#{name}-#{owner}" if description.blank?
+    self.description = "#{name} - #{owner}" if description.blank?
   end
 end
+# 
