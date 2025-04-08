0ee2742691ef9120e2747b441c6a8faf09cc5d8c
Jean Aragão <aragao.jean@gmail.com>
2025-04-07 21:29:21 -0300

    add total deposit total withdraw and balance on books list

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,6 +7,18 @@ class Book < ApplicationRecord
 
   before_validation :set_description
 
+  def total_deposit
+    daily_balances.where("balance > 0").sum(:balance)
+  end
+  
+  def total_withdraw
+    daily_balances.where("balance < 0").sum(:balance)
+  end
+  
+  def last_daily_balance
+    daily_balances.order(date: :desc).limit(1).pluck(:balance).first
+  end
+  
   def balance
     transactions.sum { |t| t.transaction_type == "withdraw" ? -t.amount : t.amount }
   end
@@ -14,6 +26,6 @@ class Book < ApplicationRecord
   private
 
   def set_description
-    self.description = "#{owner}-#{name}" if description.blank?
+    self.description = "#{name}-#{owner}" if description.blank?
   end
 end
