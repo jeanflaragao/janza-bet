562000c93c7557839e789c75777d08e337d43039
Jean Aragão <aragao.jean@gmail.com>
2025-04-13 19:42:24 -0300

    feat: add total values on index page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
     @daily_balances = DailyBalance.where(date: Date.current.beginning_of_month..Date.current)
+    @total_deposits = Transaction.where(transaction_type: 'deposit').sum(:amount)
+    @total_withdrawals = Transaction.where(transaction_type: 'withdrawal').sum(:amount)
+    @last_balance = DailyBalance.order(date: :desc).first&.balance || 0
   end
 end
