5d8a286d96f8acbddc69b8b1ce19e508927ed847
Jean Aragão <aragao.jean@gmail.com>
2025-05-01 19:17:34 -0300

    feat: add last month on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,19 +4,34 @@ class HomeController < ApplicationController
     when "month"
       start_date = Date.current.beginning_of_month
       date_range = start_date..Date.current
+    when "last_month"
+      start_date = Date.current.last_month.beginning_of_month
+      end_date = Date.current.last_month.end_of_month
+      date_range = start_date..end_date
     when "semester"
       start_date = Date.current.beginning_of_year
       date_range = start_date..Date.current
-    else # default: all time
+    else # all time
       date_range = nil
     end
 
-    @daily_balances = DailyBalance.where(date: start_date..Date.current)
-    @total_deposits = Transaction.where(transaction_type: 'deposit', date: start_date..Date.current).sum(:amount)
-    @total_withdrawals = Transaction.where(transaction_type: 'withdraw', date: start_date..Date.current).sum(:amount)
-    last_date = DailyBalance.where(date: start_date..Date.current).maximum(:date)
-    @last_balance = last_date ? DailyBalance.where(date: last_date).sum(:balance) : 0
+    transactions = Transaction.all
+    transactions = transactions.where(date: date_range) if date_range
 
-    @profit_total = (@last_balance + @total_withdrawals) - @total_deposits
+    @total_deposits = transactions.where(transaction_type: 'deposit').sum(:amount)
+    @total_withdrawals = transactions.where(transaction_type: 'withdraw').sum(:amount)
+
+    net_contribution = @total_deposits - @total_withdrawals
+
+    daily_balances = DailyBalance.all
+    daily_balances = daily_balances.where(date: date_range) if date_range
+    last_date = daily_balances.maximum(:date)
+
+    use_balance = !(params[:filter] == "month" && Date.current.day == 1)
+
+    @last_balance = last_date ? daily_balances.where(date: last_date).sum(:balance) : 0
+    @profit_total = use_balance ? (@last_balance - net_contribution) : 0
+
+    @daily_balances = daily_balances
   end
 end
