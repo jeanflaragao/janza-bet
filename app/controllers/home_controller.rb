755303e9cb6ac5380f5e41ccab8f8e4c888b9d8f
Jean Aragão <aragao.jean@gmail.com>
2025-05-11 20:42:08 -0300

    fix: total profit on home page and set month as default filter

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,18 @@
 class HomeController < ApplicationController
   def index
     case params[:filter]
-    when "month"
-      start_date = Date.current.beginning_of_month
-      end_date = Date.current
+    when "all_time"
+      start_date = nil
+      end_date = nil
     when "last_month"
       start_date = Date.current.last_month.beginning_of_month
       end_date = Date.current.last_month.end_of_month
     when "semester"
       start_date = Date.current.beginning_of_year
       end_date = Date.current
-    else # all time
-      start_date = nil
-      end_date = nil
+    else 
+      start_date = Date.current.beginning_of_month
+      end_date = Date.current
     end
 
     date_range = start_date && end_date ? (start_date..end_date) : nil
@@ -22,7 +22,6 @@ class HomeController < ApplicationController
 
     @total_deposits = transactions.where(transaction_type: 'deposit').sum(:amount)
     @total_withdrawals = transactions.where(transaction_type: 'withdraw').sum(:amount)
-    net_contribution = @total_deposits - @total_withdrawals
 
     daily_balances = DailyBalance.all
     daily_balances = daily_balances.where(date: date_range) if date_range
@@ -32,9 +31,9 @@ class HomeController < ApplicationController
     if params[:filter] == "month" && Date.current.day == 1
       use_balance = daily_balances.where(date: Date.current).exists?
     end
-
     @last_balance = last_date ? daily_balances.where(date: last_date).sum(:balance) : 0
-    @profit_total = use_balance ? (@last_balance - net_contribution) : 0
+    balance_profit = (use_balance ? @last_balance : 0) + @total_withdrawals 
+    @profit_total = balance_profit > 0 ? balance_profit - @total_deposits : 0 
 
     @daily_balances = daily_balances
   end
